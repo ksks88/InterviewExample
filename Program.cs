eeac84bd9984da55b67d0faa408669eab2427798
Salih Hajlakovic <salih.hajlakovic@gmail.com>
2017-05-24 01:05:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleAppTest
+{
+    public sealed class Circle
+    {
+        private double radius;
+
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        /// <summary>
+        /// Calculates Circumference (2*r*3.14)
+        /// </summary>
+        public double Calculate(Func<double, double> op)
+        {
+            return op(radius);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(10);
+            Console.WriteLine(circle.Calculate(calculateCircumference));
+            Console.WriteLine(circle.Calculate(r => 2 * r * 3.14));
+        }
+
+        static double calculateCircumference(double r)
+        {
+            return 2 * r * 3.14;
+        }
+    }
+}
